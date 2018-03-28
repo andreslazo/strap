d61c6f267bf399f64df544742753604e3bf8e6cd
andreslazo <am.lazo@outlook.com>
2018-03-28 13:05:11 -0300

    Test for app name in heroku

--- web/app.rb
+++ web/app.rb
@@ -39,7 +39,7 @@ get "/" do
     before_install_list_item = "<li>#{STRAP_BEFORE_INSTALL}</li>"
   end
 
-  @title = "Lemon Strap"
+  @title = "Lemon Strap Test Heroku"
   @text = <<-EOS
 Lemon Strap is a script to bootstrap a minimal macOS development system. This does not assume you're doing Ruby/Rails/web development but installs the minimal set of software every macOS developer will want.
 
